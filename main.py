c0fd73fb8049fb97d3c9bffd1468fda8b2c13c53
Houda222 <houda.ghallab@imt-atlantique.net>
2024-01-05 22:02:58 +0100

    almost done with sign up, will work now on login

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-import threading
-import copy
 from ultralytics import YOLO
 from GoGame import *
 from GoBoard import *
 from GoVisual import *
-from flask import Flask, render_template, Response, request, send_file
+from flask import render_template, Response, request, jsonify, Blueprint
 import cv2
 import base64
+from __init__ import db
+
+main = Blueprint('main', __name__)
 
 cam_index = 0
-
-app = Flask(__name__, static_url_path='/static')
-app.secret_key = 'your_secret_key'  
-
-
 
 model = YOLO('model.pt')
 
 usual_message = "Everything is OK"
 message = "Nothing is being streamed for the moment"
-# disabled_button = 'stop-button'
-
+defaut_turn = "No game is being played at the moment"
+resigned = False
 
 ProcessFrame = None
 Process = True
@@ -31,9 +27,13 @@ game_plot = empty_board
 process_thread = None
 go_game = None
 transparent_mode = False
+camera = None
 
+STARTED = False
+STOPPED = False
+SESSION_IS_OPEN = True
 
-def New_game(transparent_mode=False):
+def new_game(transparent_mode=False):
     
     global go_game, initialized, game_plot
     game = sente.Game()
@@ -43,7 +43,7 @@ def New_game(transparent_mode=False):
     game_plot = empty_board
     initialized = False
 
-def processing_thread():
+def processing_thread(ProcessFrame):
     """
         Process the detection algorithm
         
@@ -52,7 +52,7 @@ def processing_thread():
         Send error to message if there is one
         """
     
-    global ProcessFrame, game_plot, message, initialized, sgf_text
+    global game_plot, message, initialized, sgf_text
 
     if not ProcessFrame is None:
         try:
@@ -64,9 +64,9 @@ def processing_thread():
                 game_plot, sgf_text = go_game.main_loop(ProcessFrame)
                 message = usual_message
         except Exception as e:
-            message = "Error : "+str(e)
+            message = "Error : " + str(e)
                 
-def generate_plot():
+def generate_plot(frame):
     """
         Generate a plot representing the game
         
@@ -74,8 +74,9 @@ def generate_plot():
             Image
         """
     global game_plot
-    
-    processing_thread()
+
+    processing_thread(frame)
+    ###### condition deja implementé dans gogame? A revoir
     if transparent_mode:
         to_plot = game_plot
     else:
@@ -86,35 +87,102 @@ def generate_plot():
 
     return img_base64
 
-# # def end_camera():
-# #     global process_thread, camera_running, disabled_button
-# #     if camera_running:
-# #         process_thread.join()  # Wait for the thread to finish before stopping
-# #         camera_running = False
-# #         disabled_button = 'stop-button'   # Define the ID of the button to desactivate
+@main.route('/initialize_new_game')
+def initialize_new_game():
+    
+    new_game()
+    return Response(status=204)
 
-# # def open_camera():
-# #     """Open the camera"""
-# #     global camera, process_thread, disabled_button, camera_running
-# #     if not camera_running:
-# #         camera = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
-# #         process_thread = threading.Thread(target=processing_thread, args=(camera,))
-# #         process_thread.start()
-# #         camera_running = True
-# #         disabled_button = 'start-button'  # Define the ID of the button to desactivate
+# @app.route('/close_camera', methods=["POST"])
+# def close_camera():
+#     """stop the camera """
+#     global camera, STOPPED, SESSION_IS_OPEN
+    
+#     data = request.data.decode('utf-8')
+#     print(data)
+#     if data == 'false':
+#         SESSION_IS_OPEN = False
+#     else:
+#         SESSION_IS_OPEN = True
 
-@app.route('/')
-def index():
-    """Route to display HTML page"""
-    return render_template('Home.html')
+#     camera.release()
+#     STOPPED = True
+#     print("the camera has been closed")
+#     return Response(status=204)
 
-@app.route('/home')
-def home():
-    """Route to display HTML page"""
-    return render_template('Home.html')
+# @app.route('/open_camera', methods=["POST"])
+# def open_camera():
+#     """open the camera """
+#     global camera, STARTED
+#     camera = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
+#     success, frame = camera.read()
+#     if success:
+#         print("camera started")
+#         STARTED = True
+#         new_game()
+#         return Response(status=204)
+#     print("camera not opened")
+#     return Response(status=502)
 
-@app.route('/update')
-def afficher_message():
+
+@main.route('/start_play', methods=['POST'])
+def start_play():
+    new_game()
+    return Response(status=204)
+
+@main.route('/play_stone', methods=['POST'])
+def play_stone():
+    x = int(request.args.get('x'))
+    y = int(request.args.get('y'))
+    go_game.play_a_move(x, y)
+
+    return Response(status=204)
+
+@main.route('/turn', methods=['GET'])
+def show_turn():
+
+    global turn 
+        
+    if go_game.is_over() and resigned == False:
+        turn = str(go_game.get_winner()) + " wins."
+        
+    elif go_game.is_over() and resigned == True:
+        if str(go_game.get_winner()) == "BLACK":
+            turn = "WHITE resigned. BLACK wins."
+        elif str(go_game.get_winner()) == "WHITE":
+            turn = "BLACK resigned. WHITE wins."
+    else:
+        turn = str(go_game.current_turn()) + " to play"
+
+    return {'turn': turn}
+    
+@main.route('/resign', methods=['POST'])
+def resign():
+    global resigned
+    go_game.resign()
+    resigned = True
+    return Response(status=204)
+
+@main.route('/win', methods=['GET'])
+def winner():
+    return {"winner": str(go_game.get_winner())}
+
+# @app.route('/update_state')
+# def update_state():
+#     """
+#         Route to update the image and the message to display 
+        
+#         Returns:
+#             message
+#             image
+#     """
+#     global message
+
+#     return {'message': message, 'image' : generate_plot()}
+
+        
+@main.route('/update_state', methods=["POST"])
+def update_state():
     """
         Route to update the image and the message to display 
         
@@ -122,271 +190,132 @@ def afficher_message():
             message
             image
     """
+    global message
 
-    return {'message': message, 'image' : generate_plot()}
+    data = request.get_json()
+   
+    if 'image' in data:
+        try:
+            image_data_url = data['image']
+            # Extract the base64-encoded image data
+            _, image_base64 = image_data_url.split(',')
+            if image_base64:
+                image_data = base64.b64decode(image_base64)
+                nparr = np.frombuffer(image_data, np.uint8)
+                
+                # Decode the image using OpenCV
+                frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                # print(frame)
+                cv2.imwrite("test.jpg", frame)
 
-# def generate_frames():
-#     """
-#         Generate an image from the video stream
-        
-#         Returns:
-#             Image
-#     """
-#     global ProcessFrame, camera
-#     while True:  
-#         try:
-#             success, frame = camera.read()  # Read the image from the camera
-#             if not success:
-#                 break
+                return {'message': message, 'image' : generate_plot(frame)}
+        except Exception as e:
+            print(e)
             
-#             else:
-#                 ProcessFrame = copy.deepcopy(frame)
-#                 _, buffer = cv2.imencode('.jpg', frame)
-#                 frame = buffer.tobytes()
-#                 yield (b'--frame\r\n'
-#                         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#         except Exception:
-#             print('Exception: Camera not detected')
-#             break
-    
-# @app.route('/video_feed')
-# def video_feed():
-#     """
-#     Route to send the video stream 
-#     """
-#     print("in video feed")
-#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(status=502)
 
-# def end_camera():
-#     """stop the camera """
-#     global camera
-#     camera.release()
+@main.route('/get_config', methods=['GET'])
+def get_config():
+    global STARTED, STOPPED
+    config_set = {'STARTED': STARTED, 'STOPPED': STOPPED}
+    return jsonify(config_set)
 
-# def open_camera():
-#     """open the camera """
-#     global camera
-#     camera = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
-
-
-# @app.route('/cam', methods=['POST', 'GET'])
-# def getval():
-#     """
-#         Route to send the video stream 
-#     """
-#     global disabled_button, transparent_mode
-    
-#     transparent_mode = False
- 
-#     try:
-#         k = request.form['psw1']
-#         if k == '0':
-#             open_camera()
-#             if camera.read()[0]:
-#                 New_game()
-#                 disabled_button = 'start-button'
-#         elif k == '1':
-#             end_camera()
-#             disabled_button = 'stop-button'
-#     except Exception:
-#         print("Exception: Page can not be refreshed")
-    
-    
-#     return render_template('partie.html', disabled_button=disabled_button)
-
-# @app.route('/t', methods=['POST', 'GET'])
-# def getvaltransparent():
-#     """
-#     Route to send the video stream 
-#     """
-#     global disabled_button, transparent_mode
-    
-#     transparent_mode = True
-#     try:
-#         k = request.form['psw1']
-#         if k == '0':
-#             open_camera()
-#             if camera.read()[0]:
-#                 New_game(True)
-#                 disabled_button = 'start-button'
-#         elif k == '1':
-#             end_camera()
-#             disabled_button = 'stop-button'
-#     except Exception:
-#         print("Exception: Page can not be refreshed")
-        
-#     return render_template('transparent.html', disabled_button=disabled_button)
-
-# @app.route('/game', methods=['POST'])
-# def getval2():
-#     """
-#         Change the current move
-#     """
-#     global transparent_mode
-    
-#     transparent_mode = False
-    
-#     i = request.form['psw2']
-#     if i =='2':
-#         go_game.go_visual.initial_position()
-#     elif i == '3':
-#         go_game.go_visual.previous()
-#     elif i == '4':
-#         go_game.go_visual.next()
-#     elif i == '5':
-#         go_game.go_visual.final_position() 
-#     print(i)   
-#     return render_template('partie.html', disabled_button=disabled_button)
-
-@app.route('/sgf_controls')
-def getval3():
+@main.route('/controls', methods=["POST"])
+def controls():
     """
         Change the current move
     """
-    global transparent_mode
-    
-    transparent_mode = False
-    
-    i = request.form['psw2']
-    if i =='2':
+    control = request.data.decode('utf-8')
+    if control == "initial":
         go_game.go_visual.initial_position()
-    elif i == '3':
+    elif control == "previous":
         go_game.go_visual.previous()
-    elif i == '4':
+    elif control == "next":
         go_game.go_visual.next()
-    elif i == '5':
-        go_game.go_visual.final_position() 
-    print(i)   
-    # return send_file()
-    # return render_template('sgf.html', disabled_button=disabled_button)
-
-# @app.route('/rules', methods=['POST'])
-# def handle_rules():
-#     """
-#         Check if we want to apply rules, still not implemented
-#     """
-#     global rules_applied, transparent_mode
+    elif control == "last":
+        go_game.go_visual.final_position()
+    else:
+        return Response(status=500)
+    print("Control success")
     
-#     transparent_mode = False
-    
-#     rules_applied = request.form['psw3']
-#     if rules_applied == "True":
-#         go_game.set_transparent_mode(False)
-#         rules_applied = "False"
-#     else : 
-#         go_game.set_transparent_mode(True)
-#         print("########pas de regles")
-#         rules_applied = "True"
-#     return render_template('partie.html', disabled_button=disabled_button)
+    return Response(status=204)
 
-# @app.route('/change_place', methods=['POST'])
-# def change_place():
-#     """
-#         Route to get the piece that we want to change its position
-#         """
-#     global transparent_mode
-    
-#     transparent_mode = False
-    
-#     old_pos = request.form['input1']
-#     new_pos = request.form['input2']
-#     try:
-#         go_game.correct_stone(old_pos,new_pos)
-#     except Exception as e:
-#         message = "L'erreur est "+str(e)
-#     return render_template('partie.html', disabled_button=disabled_button)
-
-# @app.route('/get_sgf_txt')
-# def get_sgf_txt():
-#     """
-#         Route which returns the sgf text to be uploaded
-#         """
-#     global sgf_text
-#     return sgf_text
-
-@app.route('/upload', methods=['POST'])
+@main.route('/upload', methods=['POST'])
 def process():
     """
         Route which enables us to load the sgf text
-        """
-    global transparent_mode
-    
-    transparent_mode = False
+    """
     file = request.files['file']
     file_path = file.filename
     try:
+        new_game()
         go_game.go_visual.load_game_from_sgf(file_path)
-        message = "File charged correclty"
+        message = "Uploaded"
     except Exception as e:
-        message = "Error: "+str(e)
-    
+        message = "Error:" + str(e)
 
-    return render_template('sgf.html')
+    return Response(status=204)
 
-@app.route('/credit')
-def credit():
+@main.route('/undo', methods=['POST'])
+def undo():
     """
-        Route to get to the credit page
-        """
-    return render_template("credits.html")
+    Undo last played move
+    """
 
-# @app.route('/undo', methods=['POST'])
-# def undo():
-#     """
-#     undo last played move
-#     """
-#     global transparent_mode
+    go_game.delete_last_move()
     
-#     transparent_mode = False
-    
-#     go_game.delete_last_move()
-#     return render_template("partie.html")
-    
-@app.route('/historique')
+    return Response(status=204)
+
+@main.route('/')
+def index():
+    """Route to display HTML page"""
+    return render_template('Home.html')
+
+@main.route('/home')
+def home():
+    """Route to display HTML page"""
+    return render_template('Home.html')
+
+@main.route('/game')
+def game():
+    """
+    Route to get to the streaming page in game mode
+    """
+    if not initialized:
+        try:
+            camera.release()
+        except:
+            pass
+    return render_template("game.html")
+@main.route('/play')
+def play():
+    """
+    Route to get to the streaming page in game mode
+    """
+    return render_template("play.html")
+@main.route('/transparent')
+def transparent():
+    """
+        Route to get to the streaming page in transparent mode
+        """
+    return render_template("game.html")
+
+@main.route('/sgf')
+def sgf():
+    """
+        Route to get to the streaming page in transparent mode
+    """
+
+    return render_template("sgf.html")
+
+@main.route('/historique')
 def historique():
     """
         Route to get to the summary page
     """
     return render_template("Historique.html")
 
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
 
-@app.route('/game')
-def game():
-    """
-    Route to get to the streaming page in game mode
-    """
-    global transparent_mode
-    
-    transparent_mode = False
-
-    return render_template("game.html")
-
-@app.route('/transparent')
-def transparent():
-    """
-        Route to get to the streaming page in transparent mode
-        """
-    go_game.set_transparent_mode(True)
-    global transparent_mode
-    
-    transparent_mode = False
-    return render_template("transparent.html")
-
-@app.route('/sgf')
-def sgf():
-    """
-        Route to get to the streaming page in transparent mode
-        """
-    global transparent_mode
-    
-    transparent_mode = False
-    return render_template("sgf.html")
-
-
-
-if __name__ == '__main__':
-    New_game()
-    # process_thread = threading.Thread(target=processing_thread, args=())
-    # process_thread.start()
-    app.run(debug=True)
-    
-# %%
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from __init__ import db
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
