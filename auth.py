1d8edf3b4daec733c2558024077d6c0042cb1208
Houda222 <houda.ghallab@imt-atlantique.net>
2024-01-05 21:17:33 +0100

    woking on auth

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from __init__ import db
 
 auth = Blueprint('auth', __name__)
@@ -14,3 +14,8 @@ def signup():
 @auth.route('/logout')
 def logout():
     return 'Logout'
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
