54e481c2f57d943b3546bb1f26ea83a6f9eef608
Houda222 <houda.ghallab@imt-atlantique.net>
2024-01-05 22:20:08 +0100

    almost done with sign up, will work now on login

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
