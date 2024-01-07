c8ff116604510dfd84ca9a0f44de43955da94f11
Houda222 <houda.ghallab@imt-atlantique.net>
2024-01-07 14:00:39 +0100

    working on profile page

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -12,6 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 
 import main
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from flask import Flask, render_template, Response, request, jsonify, flash, red
 from flask_login import login_user, current_user, logout_user, login_required
 from forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from flask_login import login_required, current_user
+from flask_mail import Message
 import cv2
 import base64
 from flask_sqlalchemy import SQLAlchemy
 from models import User
-from __init__ import app, db, bcrypt
+from __init__ import app, db, bcrypt, mail
 import secrets
 from PIL import Image
 import os
